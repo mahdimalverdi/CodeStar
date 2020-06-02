5cde6e46d758486a7f209d420a18e71e1b856223
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2020-06-02 10:32:58 +0430

    change host name

--- server.ts
+++ server.ts
@@ -42,8 +42,8 @@ function run() {
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`Node Express server listening on http://0.0.0.0:${port}`);
   });
 }
 
