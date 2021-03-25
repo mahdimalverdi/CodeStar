2483cbbd8782a51a3c0e830cdf7770c0d6b8258c
BijanEisapour <bijanprogrammer@gmail.com>
2021-03-26 00:19:04 +0430

    [FIX] route animation

--- src/app/route-animation.ts
+++ src/app/route-animation.ts
@@ -1,31 +1,34 @@
 import {animate, animateChild, group, query, style, transition, trigger} from '@angular/animations';
 
-export const slideInAnimation =
-  trigger('routeAnimations', [
-    transition('* <=> *', [
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
+const slideInAnimationTiming = '0.5s ease-in-out';
+export const slideInAnimation = trigger('routeAnimations', [
+  transition('* <=> *', [
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%'
+      })
+    ], {optional: true}),
+    query(':leave', [
+      style({opacity: 1})
+    ], {optional: true}),
+    query(':enter', [
+      style({opacity: 0})
+    ], {optional: true}),
+    query(':leave', animateChild(), {optional: true}),
+    group([
+      query(':leave', [
+        animate(slideInAnimationTiming, style({opacity: 0}))
       ], {optional: true}),
       query(':enter', [
-        style({left: '-100%'})
-      ], {optional: true}),
-      query(':leave', animateChild(), {optional: true}),
-      group([
-        query(':leave', [
-          animate('200ms ease-out', style({left: '100%'}))
-        ], {optional: true}),
-        query(':enter', [
-          animate('300ms ease-out', style({left: '0%'}))
-        ], {optional: true})
-      ]),
-      query(':enter', animateChild(), {optional: true})
-    ])
-  ]);
+        animate('0.5s ' + slideInAnimationTiming, style({opacity: 1}))
+      ], {optional: true})
+    ]),
+    query(':enter', animateChild(), {optional: true})
+  ])
+]);
 
 export const preventInitialChildAnimations = trigger('preventInitialChildAnimations', [
   transition(':enter', [
