f10137d36fa71d75f7124e746cfbb9b26ee62262
BijanEisapour <bijanprogrammer@gmail.com>
2021-04-02 01:16:14 +0430

    [UPDATE] requirements

--- src/app/pages/requirements/requirements.component.ts
+++ src/app/pages/requirements/requirements.component.ts
@@ -1,15 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-requirements',
     templateUrl: './requirements.component.html',
     styleUrls: ['./requirements.component.scss'],
 })
-export class RequirementsComponent implements OnInit {
+export class RequirementsComponent {
+    public requirements: string [] = [
+        'دانشجوی کارشناسی رشتۀ مهندسی کامپیوتر یا علوم کامپیوتر',
+        'کسب نمرۀ قبولی در درس برنامه‌سازی پیشرفته (Advanced Programming)',
+        'وقت کافی در طول دوره (از اول اسفند تا آخر اردیبهشت)، ساعت ۱۷ تا ۲۰:۳۰ شنبه تا چهارشنبه',
+        'اشتیاق برای سر و کله زدن با چالش‌های فنی',
+        'لذت بردن از کار تیمی و یادگیری مهارت‌های نرم',
+    ];
     
-    constructor() { }
+    public extras: string  [ ] = [
+        'آشنایی با ساختمان داده‌ها و الگوریتم‌ها (Data Structures & Algorithms)',
+        'آشنایی با پایگاه داده (Database)',
     
-    ngOnInit(): void {
-    }
-    
+    ];
 }
