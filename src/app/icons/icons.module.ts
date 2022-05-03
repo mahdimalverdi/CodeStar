e5b9fc1054152188178ab28488ddd403a9e8ec67
BijanEisapour <bijanprogrammer@gmail.com>
2022-05-03 13:01:22 +0430

    [ADD] linkedin to footer

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -4,11 +4,12 @@ import {CommonModule} from '@angular/common';
 import {AparatComponent} from './aparat/aparat.component';
 import {FaqComponent} from './faq/faq.component';
 import {InstagramComponent} from './instagram/instagram.component';
+import {LinkedinComponent} from './linkedin/linkedin.component';
 import {TelegramComponent} from './telegram/telegram.component';
 
 @NgModule({
-    declarations: [AparatComponent, FaqComponent, InstagramComponent, TelegramComponent],
+    declarations: [AparatComponent, FaqComponent, InstagramComponent, LinkedinComponent, TelegramComponent],
     imports: [CommonModule],
-    exports: [AparatComponent, FaqComponent, InstagramComponent, TelegramComponent],
+    exports: [AparatComponent, FaqComponent, InstagramComponent, LinkedinComponent, TelegramComponent],
 })
 export class IconsModule {}
---
+++ src/app/icons/linkedin/linkedin.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'icon-linkedin',
+    templateUrl: './linkedin.component.svg',
+})
+export class LinkedinComponent {}
